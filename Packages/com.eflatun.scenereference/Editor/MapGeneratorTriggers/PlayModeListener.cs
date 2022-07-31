bf30e16f221a15deb6eb21e04828393b3b714830
starikcetin <cetinsamedtarik@gmail.com>
2022-07-31 22:32:59 +0300

    fix: only show "disabled" logs if the generation would have occurred otherwise

--- Packages/com.eflatun.scenereference/Editor/MapGeneratorTriggers/PlayModeListener.cs
+++ Packages/com.eflatun.scenereference/Editor/MapGeneratorTriggers/PlayModeListener.cs
@@ -12,16 +12,19 @@ namespace Eflatun.SceneReference.Editor.MapGeneratorTriggers
 
         private static void EditorApplication_OnPlayModeStateChanged(PlayModeStateChange state)
         {
-            if (!SettingsManager.SceneGuidToPathMap.IsGenerationTriggerEnabled(SceneGuidToPathMapGenerationTriggers.BeforeEnterPlayMode))
-            {
-                Logger.Warn($"Skipping scene GUID to path map generation before play mode. It is recommended to enable map generation before play mode, as an outdated map can result in broken scene references in runtime. You can enable it in {SettingsManager.SettingsMenuPathForDisplay}.");
-                
-                return;
-            }
-
             if (state == PlayModeStateChange.ExitingEditMode)
             {
-                SceneGuidToPathMapGenerator.Run();
+                if (SettingsManager.SceneGuidToPathMap.IsGenerationTriggerEnabled(SceneGuidToPathMapGenerationTriggers.BeforeEnterPlayMode))
+                {
+                    SceneGuidToPathMapGenerator.Run();
+                }
+                else
+                {
+                    // DESIGN: If 'Clear On Play' is enabled, this will get cleared before user gets a chance to see 
+                    // it. It is easy to fix: just log in EnteredPlayMode. But that is overengineering and I don't want
+                    // to do it.
+                    Logger.Warn($"Skipping scene GUID to path map generation before play mode. It is recommended to enable map generation before play mode, as an outdated map can result in broken scene references in runtime. You can enable it in {SettingsManager.SettingsMenuPathForDisplay}.");
+                }
             }
         }
     }
--- Packages/com.eflatun.scenereference/Editor/MapGeneratorTriggers/SceneAssetPostprocessor.cs
+++ Packages/com.eflatun.scenereference/Editor/MapGeneratorTriggers/SceneAssetPostprocessor.cs
@@ -8,13 +8,6 @@ namespace Eflatun.SceneReference.Editor.MapGeneratorTriggers
     {
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
-            if (!SettingsManager.SceneGuidToPathMap.IsGenerationTriggerEnabled(SceneGuidToPathMapGenerationTriggers.AfterSceneAssetChange))
-            {
-                Logger.Warn($"Skipping scene GUID to path map generation after scene asset changes. It is recommended to enable map generation after scene asset changes, as an outdated map can result in broken scene references in runtime. You can enable it in {SettingsManager.SettingsMenuPathForDisplay}.");
-                
-                return;
-            }
-
             var hasSceneChange = importedAssets
                 .Concat(deletedAssets)
                 .Concat(movedAssets)
@@ -23,7 +16,14 @@ namespace Eflatun.SceneReference.Editor.MapGeneratorTriggers
             
             if (hasSceneChange)
             {
-                SceneGuidToPathMapGenerator.Run();
+                if (SettingsManager.SceneGuidToPathMap.IsGenerationTriggerEnabled(SceneGuidToPathMapGenerationTriggers.AfterSceneAssetChange))
+                {
+                    SceneGuidToPathMapGenerator.Run();
+                }
+                else
+                {
+                    Logger.Warn($"Skipping scene GUID to path map generation after scene asset changes. It is recommended to enable map generation after scene asset changes, as an outdated map can result in broken scene references in runtime. You can enable it in {SettingsManager.SettingsMenuPathForDisplay}.");
+                }
             }
         }
     }
