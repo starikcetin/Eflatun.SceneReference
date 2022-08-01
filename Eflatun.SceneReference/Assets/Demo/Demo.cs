9715de14c5492795ff0767f5ec081f3cf5394105
starikcetin <cetinsamedtarik@gmail.com>
2022-08-01 04:32:39 +0300

    chore: add a demo scene to take screenshots for the repo

---
+++ Eflatun.SceneReference/Assets/Demo/Demo.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Eflatun.SceneReference;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    [SerializeField] private SceneReference sceneReference;
+    [SerializeField] private SceneReference anotherSceneReference;
+    [SerializeField] private List<SceneReference> sceneReferenceList;
+}
