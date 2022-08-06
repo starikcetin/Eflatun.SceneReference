b101adfa59850cf8040cfacd301d068ff83f9f96
starikcetin <cetinsamedtarik@gmail.com>
2022-08-07 00:12:07 +0300

    feat: change gui color depending on scene in build state

--- Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/SceneReferencePropertyDrawer.cs
+++ Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/SceneReferencePropertyDrawer.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Linq;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,29 +12,57 @@ namespace Eflatun.SceneReference.Editor
     [CustomPropertyDrawer(typeof(SceneReference))]
     public class SceneReferencePropertyDrawer : PropertyDrawer
     {
+        private SerializedProperty _sceneAssetProperty;
+        private SerializedProperty _sceneAssetGuidHexProperty;
+
+        private string SceneAssetGuidHex => _sceneAssetGuidHexProperty.stringValue;
+
+        private Object SceneAsset
+        {
+            get => _sceneAssetProperty.objectReferenceValue;
+            set
+            {
+                _sceneAssetProperty.objectReferenceValue = value;
+                
+                var sceneAssetPath = AssetDatabase.GetAssetPath(value);
+                var sceneAssetGuid = AssetDatabase.GUIDFromAssetPath(sceneAssetPath);
+                _sceneAssetGuidHexProperty.stringValue = sceneAssetGuid.ToString();
+            }
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            _sceneAssetProperty = property.FindPropertyRelative(nameof(SceneReference.sceneAsset));
+            _sceneAssetGuidHexProperty = property.FindPropertyRelative(nameof(SceneReference.sceneAssetGuidHex));
+            
+            var prevColor = GUI.color;
+            if (SceneAsset)
+            {
+                var sceneInBuildSettings = EditorBuildSettings.scenes.FirstOrDefault(x => x.guid.ToString() == SceneAssetGuidHex);
+                if (sceneInBuildSettings == null)
+                {
+                    GUI.color = Color.red;
+                }
+                else if (!sceneInBuildSettings.enabled)
+                {
+                    GUI.color = Color.yellow;
+                }   
+            }
+            
             EditorGUI.BeginProperty(position, GUIContent.none, property);
-
-            var sceneAssetProperty = property.FindPropertyRelative(nameof(SceneReference.sceneAsset));
-            var sceneAssetGuidHexProperty = property.FindPropertyRelative(nameof(SceneReference.sceneAssetGuidHex));
 
             position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
-            if (sceneAssetProperty != null && sceneAssetGuidHexProperty != null)
+            if (_sceneAssetProperty != null && _sceneAssetGuidHexProperty != null)
             {
-                var sceneAsset = EditorGUI.ObjectField(position, sceneAssetProperty.objectReferenceValue, typeof(SceneAsset), false);
-                sceneAssetProperty.objectReferenceValue = sceneAsset;
-
-                var sceneAssetPath = AssetDatabase.GetAssetPath(sceneAsset);
-                var sceneAssetGuid = AssetDatabase.GUIDFromAssetPath(sceneAssetPath);
-                sceneAssetGuidHexProperty.stringValue = sceneAssetGuid.ToString();
+                SceneAsset = EditorGUI.ObjectField(position, SceneAsset, typeof(SceneAsset), false);
             }
             else
             {
                 Logger.Error($"Could not locate properties of a {nameof(SceneAsset)} while trying to draw!");
             }
 
+            GUI.color = prevColor;
             EditorGUI.EndProperty();
         }
     }
