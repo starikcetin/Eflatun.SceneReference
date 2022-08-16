e3a7422aaaacf09477bdbc6547495d5e1cf73103
git-upm-publisher <https://github.com/starikcetin/git-upm-publisher-2>
2022-08-16 12:59:01 +0300

    UPM release 1.2.2

--- Editor/Utility/EditorUtils.cs
+++ Editor/Utility/EditorUtils.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using UnityEditor;
+﻿using System.IO;
 
 namespace Eflatun.SceneReference.Editor.Utility
 {
@@ -10,47 +7,9 @@ namespace Eflatun.SceneReference.Editor.Utility
     /// </summary>
     internal static class EditorUtils
     {
-        private const BindingFlags AllBindingFlags = (BindingFlags)(-1);
-
         /// <summary>
         /// Returns true if the given <paramref name="path"/> ends with the file extension ".unity".
         /// </summary>
         public static bool IsSceneAssetPath(this string path) => Path.GetExtension(path) == ".unity";
-
-        /// <summary>
-        /// Returns attributes of type <typeparamref name="TAttribute"/> on <paramref name="serializedProperty"/>.
-        /// </summary>
-        public static TAttribute[] GetAttributes<TAttribute>(this SerializedProperty serializedProperty, bool inherit)
-            where TAttribute : Attribute
-        {
-            if (serializedProperty == null)
-            {
-                throw new ArgumentNullException(nameof(serializedProperty));
-            }
-
-            var targetObjectType = serializedProperty.serializedObject.targetObject.GetType();
-
-            if (targetObjectType == null)
-            {
-                throw new ArgumentException($"Could not find the {nameof(targetObjectType)} of {nameof(serializedProperty)}");
-            }
-
-            foreach (var pathSegment in serializedProperty.propertyPath.Split('.'))
-            {
-                var fieldInfo = targetObjectType.GetField(pathSegment, AllBindingFlags);
-                if (fieldInfo != null)
-                {
-                    return (TAttribute[])fieldInfo.GetCustomAttributes<TAttribute>(inherit);
-                }
-
-                var propertyInfo = targetObjectType.GetProperty(pathSegment, AllBindingFlags);
-                if (propertyInfo != null)
-                {
-                    return (TAttribute[])propertyInfo.GetCustomAttributes<TAttribute>(inherit);
-                }
-            }
-
-            throw new ArgumentException($"Could not find the field or property of {nameof(serializedProperty)}");
-        }
     }
 }
