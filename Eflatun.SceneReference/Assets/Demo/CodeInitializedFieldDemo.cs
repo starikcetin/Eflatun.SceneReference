27f32056302e4d8c866cbb581dd4d78f47be6051
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2023-02-22 20:52:16 +0100

    feat: add public constructors & factory methods to SceneReference (#34)
    * fix: conditionally import UnityEditor namespace in SceneReference
    
    * style: reorder SceneReference members
    
    * feat: add public constructors & factory methods to SceneReference

---
+++ Eflatun.SceneReference/Assets/Demo/CodeInitializedFieldDemo.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using Eflatun.SceneReference;
+using UnityEngine;
+
+public class CodeInitializedFieldDemo : MonoBehaviour
+{
+    [SerializeField] private SceneReference sceneCSource;
+
+    // Assets/Scenes/SceneA.unity
+    [Space]
+    [Header("Press start and watch sceneA, sceneB, and sceneC get filled in.")]
+    [SerializeField] private SceneReference sceneA;
+
+    // Assets/Scenes/SceneB.unity
+    [SerializeField] private SceneReference sceneB;
+
+    // Assets/Scenes Folder 2/SceneC.unity
+    [SerializeField] private SceneReference sceneC;
+
+    private void Start()
+    {
+        sceneA = new SceneReference("45ac7250dffb26f4e84eda01a52b8b19");
+        sceneB = SceneReference.FromScenePath("Assets/Scenes/SceneB.unity");
+        sceneC = new SceneReference(GetSceneAsset(sceneCSource));
+    }
+
+    private static UnityEngine.Object GetSceneAsset(SceneReference x)
+    {
+        const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+        return (UnityEngine.Object) typeof(SceneReference).GetField("sceneAsset", bindingFlags)?.GetValue(x);
+    }
+}
