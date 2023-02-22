df2148d7c02df68817cd345b427dbab1084593f2
starikcetin <cetinsamedtarik@gmail.com>
2023-02-23 01:33:24 +0300

    test: prepare test assembly

--- Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/AssemblyInfo.cs
+++ Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine.Scripting;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine.Scripting;
+
+// Make internals visible to the Test assembly
+[assembly: InternalsVisibleTo("Eflatun.SceneReference.Tests")]
 
 // Always link to make sure InitializeOnLoad methods don't get stripped away
 [assembly: AlwaysLinkAssembly]
--- Eflatun.SceneReference/Packages/com.eflatun.scenereference/Runtime/AssemblyInfo.cs
+++ Eflatun.SceneReference/Packages/com.eflatun.scenereference/Runtime/AssemblyInfo.cs
@@ -4,5 +4,8 @@ using UnityEngine.Scripting;
 // Make internals visible to the Editor assembly
 [assembly: InternalsVisibleTo("Eflatun.SceneReference.Editor")]
 
+// Make internals visible to the Test assembly
+[assembly: InternalsVisibleTo("Eflatun.SceneReference.Tests")]
+
 // Always link to make sure RuntimeInitializeOnLoadMethod methods don't get stripped away
 [assembly: AlwaysLinkAssembly]
