735fdaa2eb8c7051e40867d1c52cab980aead04e
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2023-07-22 19:02:40 +0200

    fix: workaround unity bug while drawing our settings in 2022.3.4f1 (#60)

---
+++ Eflatun.SceneReference/Assets/Development Utils/Editor/SettingsManipulator.cs
@@ -0,0 +1,29 @@
+﻿using Eflatun.SceneReference.Editor;
+using UnityEditor;
+
+namespace Eflatun.SceneReference.DevelopmentUtils.Editor
+{
+    public class SettingsManipulator : EditorWindow
+    {
+        [MenuItem("Tools/" + Constants.MenuPrefixBase + "/_Dev/Settings Manipulator")]
+        public static void Display()
+        {
+            GetWindow<SettingsManipulator>();
+        }
+
+        private void OnGUI()
+        {
+            // Draw GenerationTriggers field.
+            {
+                var oldPublic = SettingsManager.SceneDataMaps.GenerationTriggers.value;
+                var oldInternal = (InternalSceneDataMapsGeneratorTriggers)oldPublic;
+                var newInternal = EditorGUILayout.EnumFlagsField("Generation Triggers", oldInternal);
+                var newPublic = (SceneDataMapsGeneratorTriggers)newInternal;
+                if (newPublic != oldPublic)
+                {
+                    SettingsManager.SceneDataMaps.GenerationTriggers.value = newPublic;
+                }
+            }
+        }
+    }
+}
--- Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/AssemblyInfo.cs
+++ Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine.Scripting;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine.Scripting;
+
+[assembly: InternalsVisibleTo("Eflatun.SceneReference.DevelopmentUtils.Editor")]
 
 // Always link to make sure InitializeOnLoad methods don't get stripped away
 [assembly: AlwaysLinkAssembly]
