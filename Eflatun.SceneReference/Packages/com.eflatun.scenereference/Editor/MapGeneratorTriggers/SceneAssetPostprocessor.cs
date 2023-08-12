7b9dd7ca56989c4aca8f1ae8658f6277c337b5dd
Ale Barbero <alejandro.barbero@gmail.com>
2023-08-12 23:38:31 +0100

    fix: delay sceneDataMapsGenerator until unity editor has finish3ed compiling

--- Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/MapGeneratorTriggers/SceneAssetPostprocessor.cs
+++ Eflatun.SceneReference/Packages/com.eflatun.scenereference/Editor/MapGeneratorTriggers/SceneAssetPostprocessor.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using Eflatun.SceneReference.Editor.Utility;
 using UnityEditor;
 
@@ -16,14 +16,31 @@ namespace Eflatun.SceneReference.Editor.MapGeneratorTriggers
 
             if (hasSceneChange)
             {
-                if (SettingsManager.SceneDataMaps.IsGenerationTriggerEnabled(SceneDataMapsGeneratorTriggers.AfterSceneAssetChange))
-                {
-                    SceneDataMapsGenerator.Run();
-                }
-                else
-                {
-                    EditorLogger.Warn($"Skipping scene data maps generation after scene asset changes. It is recommended to enable map generation after scene asset changes, as an outdated map can result in broken scene references in runtime. You can enable it in {SettingsManager.SettingsMenuPathForDisplay}.");
-                }
+                //Schedule a Map generation
+                EditorApplication.update += DelayedMapGeneration;
+            }
+        }
+
+        /// <summary>
+        /// Waits for Unity editor to finish compiling.
+        /// When that condition is met, unsubscribes from unity editor update and refreshes ScenesDataMaps.
+        /// </summary>
+        private static void DelayedMapGeneration()
+        {
+            if (EditorApplication.isCompiling || EditorApplication.isUpdating)
+            {
+                return;
+            }
+            EditorApplication.update -= DelayedMapGeneration;
+            
+
+            if (SettingsManager.SceneDataMaps.IsGenerationTriggerEnabled(SceneDataMapsGeneratorTriggers.AfterSceneAssetChange))
+            {
+                SceneDataMapsGenerator.Run();
+            }
+            else
+            {
+                EditorLogger.Warn($"Skipping scene data maps generation after scene asset changes. It is recommended to enable map generation after scene asset changes, as an outdated map can result in broken scene references in runtime. You can enable it in {SettingsManager.SettingsMenuPathForDisplay}.");
             }
         }
     }
